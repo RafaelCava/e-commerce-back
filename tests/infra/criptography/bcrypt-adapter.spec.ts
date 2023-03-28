84a10a0e09c4704118cdad0cd471e436b42d4d10
Rafael Cavalcante <rafael.cavalcante@tallos.com.br>
2023-03-28 13:21:26 -0300

    feat(bcrypt-adapter): ensure BcryptAdapter return a valid hash on succeed

--- tests/infra/criptography/bcrypt-adapter.spec.ts
+++ tests/infra/criptography/bcrypt-adapter.spec.ts
@@ -1,6 +1,16 @@
 import { BcryptAdapter } from '../../../src/infra/criptography'
 import bcrypt from 'bcrypt'
 
+jest.mock('bcrypt', () => ({
+  async hash (): Promise<string> {
+    return await Promise.resolve('hash')
+  },
+
+  async compare (): Promise<boolean> {
+    return await Promise.resolve(true)
+  }
+}))
+
 const salt = 12
 
 const makeSut = (salt: number): BcryptAdapter => {
@@ -15,6 +25,12 @@ describe('bcrypt adapter', () => {
       await sut.hash('any_value')
       expect(hashSpy).toHaveBeenCalledTimes(1)
       expect(hashSpy).toHaveBeenCalledWith('any_value', salt)
+    })
+
+    it('should return a valid hash on hash success', async () => {
+      const sut = makeSut(salt)
+      const hash = await sut.hash('any_value')
+      expect(hash).toBe('hash')
     })
   })
 })
